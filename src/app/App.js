6f3924ac53b1314b74ff97c72a0b9e1853589c70
7zK <denfore0120@gmail.com>
2024-05-05 06:30:50 +0900

    A little bit change

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home, About, Contact } from './components';
+import { Home, About, Contact } from '../components';
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
